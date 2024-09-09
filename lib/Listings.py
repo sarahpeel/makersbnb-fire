866d3580f81beaedb3018bb38f9697d5817422e3
Dewi Afoko <dewi.afoko@gmail.com>
2024-09-09 15:42:31 +0100

    Listings and Test_listings complete
    closes #2

--- lib/Listings.py
+++ lib/Listings.py
@@ -1,12 +1,13 @@
 class Listing:
 
-    def __init__(self, id, name, description, location, price):
+    def __init__(self, id, name, description, location, price, occupied=False):
         self.id = id
         self.name = name
         self.description = description
         self.location = location
         self.price = price
         self.occupied = False
+        self.availability = []
 
     def __eq__(self, other):
         return self.__dict__ == other.__dict__
@@ -15,5 +16,5 @@ class Listing:
         return f'Listing({self.id}, {self.name}, {self.description}, {self.location}, {self.price}, {self.occupied}, {self.availability})'
     
     def add_availability(self, availability):
-        self.availability = availability
+        self.availability.append(availability)
         
---
+++ tests/test_Listings.py
@@ -0,0 +1,23 @@
+from lib.Listings import Listing
+
+def test_listing_creates_correct_variables():
+    listing1 = Listing(1, "test1", "fake description 1", "location1", 5)
+    assert listing1.name == "test1"
+    assert listing1.id == 1
+    assert listing1.description == "fake description 1"
+    assert listing1.price == 5
+
+def test_add_availability():
+    listing1 = Listing(1, "test1", "fake description 1", "location1", 5)
+    listing1.add_availability(1234)
+    assert listing1.availability == [1234]
+
+def test_repr_works_with_avaialability():
+    listing2 = Listing(2, "test1", "fake description 1", "location1", 5)
+    listing2.add_availability(5678)
+    assert listing2.__repr__() == 'Listing(2, test1, fake description 1, location1, 5, False, [5678])'
+
+def test_repr_works_with_avaialability_no_dupe_lists():
+    listing1 = Listing(1, "test1", "fake description 1", "location1", 5)
+    listing1.add_availability(5678)
+    assert listing1.__repr__() == 'Listing(1, test1, fake description 1, location1, 5, False, [5678])'
