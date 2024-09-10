56f6c38ea9d8d6841b89308dd8932704b607f9dc
Andy <abartholomew2903@gmail.com>
2024-09-10 11:27:44 +0100

    add seed data & remove availibility element

--- lib/ListingsRepository.py
+++ lib/ListingsRepository.py
@@ -8,21 +8,21 @@ class ListingRepository:
 
 
     def add_listing(self, listing):
-        self._connection.execute('INSERT INTO listings (name, description, location, price, occupied, availability) VALUES (%s, %s, %s, %s, %s, %s)', (listing.name, listing.description, listing.location, listing.price, listing.occupied, listing.availability))
+        self._connection.execute('INSERT INTO listings (name, description, location, price, user_id) VALUES (%s, %s, %s, %s, %s)', [listing.name, listing.description, listing.location, listing.price, listing.user_id])
 
 
     def all(self):
-        rows = self._connection.execute("SELECT * FROM listings")
+        rows = self._connection.execute('SELECT * FROM listings')
 
         listings = []
         for row in rows:
-            item = Listing(row['id'], row['name'], row['description'], row['location'], row['price'], row['occupied'])
+            item = Listing(row['id'], row['name'], row['description'], row['location'], row['price'], row['user_id'])
             listings.append(item)
 
         return listings
     
     def find(self, listing_id):
-        rows = self._connection.execute("SELECT * FROM listings WHERE id = %s", [listing_id])
+        rows = self._connection.execute('SELECT * FROM listings WHERE id = %s', [listing_id])
 
-        return Listing(rows[0]['id'], rows[0]['name'], rows[0]['description'], rows[0]['location'], rows[0]['price'], rows[0]['occupied'])
+        return Listing(rows[0]['id'], rows[0]['name'], rows[0]['description'], rows[0]['location'], rows[0]['price'], rows[0]['user_id'])
         
