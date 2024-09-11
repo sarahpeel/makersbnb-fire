565c3fa4d669bb3a0aa69ea4991d2d668af843fa
Rebecca Clarke <rclarkeweb@hotmail.com>
2024-09-11 17:00:41 +0100

    Create new find booking method and tests

--- lib/BookingsRepository.py
+++ lib/BookingsRepository.py
@@ -1,4 +1,6 @@
 from lib.Bookings import *
+from lib.database_connection import *
+from lib.Listings import *
 
 class BookingRepository():
 
@@ -12,10 +14,32 @@ class BookingRepository():
         bookings = []
         for row in rows:
             item = Booking(row['id'], row['listing_id'], row['requester_id'], row['start_date'], row['end_date'], row['status'])
-            print(type(item.start_date))
+
             bookings.append(item)
 
         return bookings
+    
+    def find_booking_by_requester_id(self, requester_id):
+        rows = self._connection.execute('SELECT * FROM bookings WHERE requester_id = %s', [requester_id])
+        
+        bookings = []
+        for row in rows:
+            item = Booking(row['id'], row['listing_id'], row['requester_id'], row['start_date'], row['end_date'], row['status'])
+            bookings.append(item)
+
+        return bookings
+    
+    def find_booking_by_listing_user(self, user_id):
+        rows = self._connection.execute('SELECT listings.id AS listings_id, listings.name, listings.description, listings.location, listings.price, listings.user_id, bookings.id AS bookings_id, bookings.listing_id, bookings.requester_id, bookings.start_date, bookings.end_date, bookings.status FROM bookings JOIN listings ON bookings.listing_id = listings.id WHERE listings.user_id = %s', [user_id])
+
+        bookings = []
+        for row in rows:
+            item = Booking(row['bookings_id'], row['listing_id'], row['requester_id'], row['start_date'], row['end_date'], row['status'])
+            bookings.append(item)
+
+        return Listing(rows[0]['listings_id'], rows[0]['name'], rows[0]['description'], rows[0]['location'], rows[0]['price'], rows[0]['user_id'], bookings)
+        
+
         
     def is_available(self, booking):
         pass
--- tests/test_BookingsRepository.py
+++ tests/test_BookingsRepository.py
@@ -10,6 +10,35 @@ def test_find_booking(db_connection):
     x = datetime.date(2023, 6, 15)
     y = datetime.date(2023, 6, 20)
     
-    found = repo.find_booking(2)
+    found = repo.find_booking_by_listing(2)
 
     assert found == [Booking(2, 2, 3, x, y, 'confirmed')]
+
+
+
+
+def test_find_booking_by_listing_user(db_connection):
+    db_connection.seed("seeds/bnb_db.sql")
+    repo = BookingRepository(db_connection)
+
+    x = datetime.date(2023, 12, 10)
+    y = datetime.date(2023, 12, 10)
+
+    result = repo.find_booking_by_listing_user(2)
+
+    assert result == Listing(3, 'Windsor Castle', 'Really old', 'Windsor', 1150, 2, [Booking(3, 3, 1, x, y, 'requested')])
+
+
+def test_find_multiple_bookings_by_listing_user(db_connection):
+    db_connection.seed("seeds/bnb_db.sql")
+    repo = BookingRepository(db_connection)
+
+    listing1_start = datetime.date(2023, 9, 10)
+    listing1_end = datetime.date(2023, 9, 12)
+    
+    listing2_start = datetime.date(2023, 6, 15)
+    listing2_end = datetime.date(2023, 6, 20)
+
+    result = repo.find_booking_by_listing_user(1)
+
+    assert result == [Listing(1, 'Blackpool Tower', 'Really high', 'Blackpool', 25, 1, [Booking(1, 1, 2, listing1_start, listing1_end, 'requested')]), Listing(2, 'London Eye', 'Really round', 'London', 150, 1, [Booking(2, 2, 3, listing2_start, listing2_end, 'confirmed')])]
